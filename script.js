1b82603de632af5a81e14292aa814fa7a5f5e22f
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-06-22 17:19:03 +0300

    Added fetching of the data
    Fetching of the data and some raw attribution of the text.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const htmlEl = document.querySelector("html");
 const modeTogglerBtn = document.querySelector(".mode-toggler");
 const modeTogglerText = document.querySelector(".mode-state-text");
+const formEl = document.querySelector("form");
+const userDataToFullfill = document.querySelectorAll("[class^='user-git']");
 
 modeTogglerBtn.addEventListener("click", toggleMode);
 
@@ -19,4 +21,54 @@ function toggleMode() {
   modeTogglerText.textContent = moonIcon.classList.contains("show-img")
     ? "dark"
     : "light";
+}
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const rawData = new FormData(formEl);
+  const { "input-name": inputName } = Object.fromEntries(rawData);
+  const nameTrim = inputName.trim();
+  getData(nameTrim);
+});
+
+async function getData(name) {
+  const rawData = await fetch(` https://api.github.com/users/${name}`);
+
+  const response = await rawData.json();
+  console.log(response);
+  const data = [
+    response.avatar_url,
+    response.name,
+    response.login,
+    response.created_at,
+    response.bio,
+    response.public_repos,
+    response.followers,
+    response.following,
+    response.location,
+    response.twitter_username,
+    response.blog,
+    response.company,
+  ];
+
+  updatePageData(data);
+}
+
+function updatePageData(data) {
+  console.log(userDataToFullfill);
+  for (let i = 0; i < userDataToFullfill.length; i++) {
+    let text;
+    console.log(i, data[i]);
+    if (!data[i]) {
+      text = "Not Available";
+    } else {
+      text = data[i];
+    }
+
+    if (i === 0 || i === 10) {
+      userDataToFullfill[i].href = `${text}`;
+    } else {
+      userDataToFullfill[i].textContent = `${text}`;
+    }
+  }
 }
